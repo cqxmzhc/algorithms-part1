6953f7fb6d4c34cb259e2ec45cba95abd13cbf23
cqxmzhc <ldzslds@gmail.com>
2017-07-31 11:07:50 +0800

    bug fixes

--- week1/analysis-of-algorithms/Interview-Questions/Search-in-a-bitonic-array/Search-in-a-bitonic-array.go
+++ week1/analysis-of-algorithms/Interview-Questions/Search-in-a-bitonic-array/Search-in-a-bitonic-array.go
@@ -42,7 +42,12 @@ func main() {
 	fmt.Printf("bitonic array:%+v\n", r)
 
 	// bitonicSearch3lgn(r, 88)
+
 	bitonicSearch2lgn(r, 88)
+	bitonicSearch2lgn(r, 1)
+	bitonicSearch2lgn(r, 99)
+	bitonicSearch2lgn(r, 3)
+	bitonicSearch2lgn(r, 22)
 }
 
 //先通过binary search找到最大值O(lgn), 再分别从两个子数组中查找O(2lgn) => O(3lgn)
@@ -57,17 +62,17 @@ func bitonicSearch3lgn(p []int, find int) {
 	for {
 		mid := (hi + lo) / 2
 
-		max := p[mid]
+		middle := p[mid]
 		left := p[mid-1]
 		right := p[mid+1]
 
-		if left < max && max > right {
+		if left < middle && middle > right {
 			maxIndex = mid
-			fmt.Printf("maxIndex:%d; max value:%d\n", mid, max)
+			fmt.Printf("maxIndex:%d; middle value:%d\n", mid, middle)
 			break
-		} else if left < max && max < right {
+		} else if left < middle && middle < right {
 			lo = mid + 1
-		} else if left > max && max > right {
+		} else if left > middle && middle > right {
 			hi = mid - 1
 		}
 	}
@@ -79,7 +84,7 @@ func bitonicSearch3lgn(p []int, find int) {
 		mid := (hi + lo) / 2
 
 		if find == p[mid] {
-			fmt.Printf("find value index:%d\n", mid)
+			fmt.Printf("find value %d at index:%d\n", find, mid)
 			return
 		} else if hi == lo {
 			fmt.Println("left no match")
@@ -98,7 +103,7 @@ func bitonicSearch3lgn(p []int, find int) {
 		mid := (hi + lo) / 2
 
 		if find == p[mid] {
-			fmt.Printf("find value index:%d\n", mid)
+			fmt.Printf("find value %d at index:%d\n", find, mid)
 			return
 		} else if hi == lo {
 			fmt.Println("right no match")
@@ -111,7 +116,7 @@ func bitonicSearch3lgn(p []int, find int) {
 	}
 }
 
-func bitonicSearch2lgn(p []int, find int) {
+func bitonicSearch2lgn(p []int, find int) bool {
 	pl := len(p)
 
 	lo := 0
@@ -120,78 +125,84 @@ func bitonicSearch2lgn(p []int, find int) {
 	for {
 		mid := (hi + lo) / 2
 
-		max := p[mid]
+		middle := p[mid]
 		left := p[mid-1]
 		right := p[mid+1]
 
-		if find == max {
-			fmt.Printf("find value index:%d;\n")
-			break
+		if find == middle {
+			fmt.Printf("find value %d at index:%d\n", find, mid)
+			return true
 		} else if lo == hi {
-			fmt.Println("-------------")
 			fmt.Println("no match")
-			break
-		} else if left < max && max < right {
-			fmt.Println("-------------")
+			return false
+		} else if left < middle && middle < right {
 			//最大值在右边
-			if find > max {
+			if find > middle {
+				//递归执行bitonic search
 				lo = mid + 1
-			} else if find < max {
-				for {
-					t := mid + 1
-					if binarySearch(t, hi, p, find, false) {
-						return
-					}
+			} else if find < middle {
+				//期望值在左右都有可能
+				//左边，因为最大值在右边，所以左边的数组为递增的数组，使用binary search,时间复杂度O(lgn)
+				//右边，因为期望值小于mid位置的值，且mid值到max的值都大于期望值，max右边的数组是递减的数组，所有可以把mid位置右边的整个数组当作排序递减数组使用binary search,时间复杂度为O(lgn)
+
+				//所以最坏情况是先在左边没有找到，再到右边搜索，时间复杂度为O(2lgn)
+				t := mid + 1
+				if binarySearch(t, hi, p, find, false) {
+					return true
 				}
 
-				for {
-					t := mid - 1
-					binarySearch(lo, t, p, find, true)
-				}
+				t = mid - 1
+				return binarySearch(lo, t, p, find, true)
 			}
-		} else if left > max && max > right {
+		} else if left > middle && middle > right {
 			//最大值在左边
-			if find > max {
+			if find > middle {
 				hi = mid - 1
-			} else if find < max {
-				for {
-					t := mid - 1
-					if binarySearch(lo, t, p, find, true) {
-						return
-					}
+			} else if find < middle {
+				t := mid - 1
+				if binarySearch(lo, t, p, find, true) {
+					return true
 				}
 
-				for {
-					t := mid + 1
-					binarySearch(t, hi, p, find, false)
-				}
+				t = mid + 1
+				return binarySearch(t, hi, p, find, false)
 			}
+		} else if left < middle && middle > right {
+			//取到最大值
+
+			t := mid - 1
+			if binarySearch(lo, t, p, find, true) {
+				return true
+			}
+
+			t = mid + 1
+			return binarySearch(t, hi, p, find, false)
 		}
 	}
 }
 
 func binarySearch(lo, hi int, p []int, find int, asc bool) bool {
-	mid := (lo + hi) / 2
-
 	for {
+		mid := (lo + hi) / 2
+
 		if find == p[mid] {
-			fmt.Println("find value index:%d\n", mid)
+			fmt.Printf("find value %d at index:%d\n", find, mid)
 			return true
 		} else if lo == hi {
-			fmt.Println("no match")
+			fmt.Println("left no match")
 			return false
 		}
 
 		if asc {
-			if find > mid {
+			if find > p[mid] {
 				lo = mid + 1
-			} else if find < mid {
+			} else if find < p[mid] {
 				hi = mid - 1
 			}
 		} else {
-			if find > mid {
+			if find > p[mid] {
 				hi = mid - 1
-			} else if find < mid {
+			} else if find < p[mid] {
 				lo = mid + 1
 			}
 		}
