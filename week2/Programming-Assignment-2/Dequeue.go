891d6b2ec4a9de274eaed0d36360af63c1a4c8cd
cqxmzhc <ldzslds@gmail.com>
2017-08-07 20:50:48 +0800

    dequeue

--- week2/Programming-Assignment-2/Dequeue.go
+++ week2/Programming-Assignment-2/Dequeue.go
@@ -1,6 +1,41 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	d := Deque()
+	for i := 0; i < 10; i++ {
+		d.addFirst(i)
+	}
+	fmt.Printf("number of deque:%d\n", d.size())
+
+	for i := 10; i < 20; i++ {
+		d.addLast(i)
+	}
+	fmt.Println("number of deque:", d.size())
+
+	i := d.iterator()
+	for v := range i {
+		fmt.Printf("%d ", v.value)
+		fmt.Println()
+	}
+
+	d.removeLast()
+	i = d.iterator()
+	for v := range i {
+		fmt.Printf("%d ", v.value)
+		fmt.Println()
+	}
+
+	d.removeFirst()
+	i = d.iterator()
+	for v := range i {
+		fmt.Printf("%d ", v.value)
+		fmt.Println()
+	}
+
 }
 
 type dequeNode struct {
@@ -17,7 +52,7 @@ type deque struct {
 
 func Deque() *deque {
 	return &deque{
-		size:  0,
+		num:   0,
 		first: nil,
 		last:  nil,
 	}
@@ -38,8 +73,17 @@ func (d *deque) addFirst(v int) {
 		previous: nil,
 	}
 
-	n.next = d.first
-	d.first = n
+	if d.last == nil {
+		d.last = n
+	}
+	if d.first == nil {
+		d.first = n
+	} else {
+		d.first.previous = n
+		n.next = d.first
+		d.first = n
+	}
+
 	d.num += 1
 }
 
@@ -50,10 +94,16 @@ func (d *deque) addLast(v int) {
 		previous: nil,
 	}
 
+	if d.first == nil {
+		d.first = n
+	}
+
 	if d.last == nil {
 		d.last = n
 	} else {
 		d.last.next = n
+		n.previous = d.last
+		d.last = n
 	}
 
 	d.num += 1
@@ -62,18 +112,47 @@ func (d *deque) addLast(v int) {
 func (d *deque) removeFirst() {
 	if d.first != nil {
 		d.first = d.first.next
+		if d.first != nil {
+			d.first.previous = nil
+		}
 		d.num -= 1
 	}
 
 }
 
 func (d *deque) removeLast() {
+	fmt.Println("d.last:", d.last.value)
+	fmt.Println("d.last.previous:", d.last.previous.value)
 	if d.last != nil {
 		d.last = d.last.previous
+		if d.last != nil {
+			d.last.next = nil
+		}
 		d.num -= 1
 	}
 }
 
 func (d *deque) iterator() chan *dequeNode {
+	c := make(chan *dequeNode, d.num)
 
+	go func() {
+		if d.first == nil {
+			close(c)
+			return
+		}
+
+		n := d.first
+		for {
+			if n != nil {
+				c <- n
+				n = n.next
+			} else {
+				break
+			}
+		}
+
+		close(c)
+	}()
+
+	return c
 }
